341a228439c0914fcff8ac9ab9210b8e74a724c8
yusharth <yusharthsingh@gmail.com>
2024-01-08 15:05:19 +0530

    server stuff uploaded

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -71,6 +71,15 @@ TEMPLATES = [
 ]
 ASGI_APPLICATION = "ChatApp.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 WSGI_APPLICATION = 'ChatApp.wsgi.application'
 
 
